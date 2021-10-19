74713139df5a960202afff4f4bcc23e90b36b8a1
Mateusz Malinowski <malina314@op.pl>
2021-10-19 15:04:50 +0200

    dodanie asercji

--- maptel.cc
+++ maptel.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstring>
 #include <cassert>
+#include <regex>
 
 using namespace std;
 
@@ -23,36 +24,55 @@ using maptel_id_t = unsigned long;
 //pytanie: czy nazwa dicts jest dobra?
 unordered_map<maptel_id_t, unordered_map<string, string>> dicts;
 
+// Zakładamy, że poprawny telefon jest niepusty.
+// (chyba że pusty jest poprawny to wtedy powinno być \\d*)
+regex tel_nr("\\d+");
+
 unsigned long maptel_create(void) {
     static maptel_id_t dict_idx = 0;
     return dict_idx++;
 }
 
 void maptel_delete(maptel_id_t id) {
+    assert(dicts.count(id) != 0);
     dicts.erase(id);
 }
 
 void maptel_insert(maptel_id_t id, char const *tel_src, char const *tel_dst) {
-    //todo: spr czy id istnieje w mapie
-    //todo: spr czy długość nie przekracza limitu 22
+    assert(dicts.count(id) != 0);
     assert(strlen(tel_src) <= TEL_NUM_MAX_LEN);
+    assert(strlen(tel_dst) <= TEL_NUM_MAX_LEN);
+
     string src(tel_src), dst(tel_dst);
+
+    assert(regex_match(src, tel_nr));
+    assert(regex_match(dst, tel_nr));
+
     dicts[id][src] = dst;
 }
 
 void maptel_erase(maptel_id_t id, char const *tel_src) {
-    //todo: spr czy id istnieje w mapie
-    //todo: spr czy długość nie przekracza limitu 22
+    assert(dicts.count(id) != 0);
+    assert(strlen(tel_src) <= TEL_NUM_MAX_LEN);
+
     string src(tel_src);
+
+    assert(regex_match(src, tel_nr));
+    assert(dicts[id].count(src) != 0);
+
     dicts[id].erase(src);
 }
 
 void maptel_transform(maptel_id_t id, char const *tel_src, char *tel_dst,
                       size_t len) {
+    assert(dicts.count(id) != 0);
+    assert(strlen(tel_src) <= TEL_NUM_MAX_LEN);
+
     unordered_set<string> cycle;
-    //todo: spr czy id istnieje w mapie
-    //todo: spr czy długość nie przekracza limitu 22
-    string src(tel_src), dst(tel_dst);
+    string src(tel_src), dst;
+
+    assert(regex_match(src, tel_nr));
+
     //todo: zrobić tak żeby nie kopiować
     while (dicts[id].count(dst)) {
         src = dst;
